8f1cfe50cecea6bb39d581afd0eca9ab5f7bf9e9
rij1234 <69735277+rij1234@users.noreply.github.com>
2021-05-05 12:42:59 -0700

    Update Config.java

--- src/main/java/com/rij1234/DiscordBridgeMod/Config.java
+++ src/main/java/com/rij1234/DiscordBridgeMod/Config.java
@@ -1,5 +1,8 @@
 package com.rij1234.DiscordBridgeMod;
 
 public class Config {
-
+    public static String WebhookUrl = "wh_url";
+    public static String DiscordBotToken = "dis_bot_token";
+    public static String ServerId = "server_id_as_string";
+    public static long channel_id = 8237492387456230L; // channel id with an "L" at the end
 }
